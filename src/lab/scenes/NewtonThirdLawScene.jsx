9b8e3ab725bd06fcaec877d6b3f211e29b6525e8
cuag313 <cuag313@outlook.com>
2026-09-08 07:12:02 +0800

    磁铁接触距离微调至1.1

--- src/lab/scenes/NewtonThirdLawScene.jsx
+++ src/lab/scenes/NewtonThirdLawScene.jsx
@@ -53,7 +53,7 @@ export default function NewtonThirdLawScene() {
     magStr: 5,          // 磁力强度
     magPhase: 'idle',
     magForce: 0,
-    magContactDist: 0.9, // 接触距离
+    magContactDist: 1.1, // 接触距离
 
     // 通用
     time: 0,
